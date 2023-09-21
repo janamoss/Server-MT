f9a3131b3412fc1c294b1f8278c93a32a2f5b6c3
jimimoss <lowzaokmi248@gmail.com>
2023-09-21 18:12:46 +0700

    create orderdetail

---
+++ models/OrderDetail.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const orderdetailSchema = new mongoose.Schema({
+    SKUs_idSKUs: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SKUs'
+    },
+    Orders_idOrders: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order'
+    },
+    size: Number,
+    qty: Number
+});
+
+module.exports = mongoose.model('OrderDetail', orderdetailSchema);

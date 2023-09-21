9a1e58e51e85f96e1617e2b1da90812f2d63f787
jimimoss <lowzaokmi248@gmail.com>
2023-09-21 15:42:29 +0700

    connect MongoDb & model & routes

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
 const cors = require("cors");
 const PORT = 8080;
 
+const dbUrl = "mongodb+srv://johnny:test1234@cluster0.e9jvqih.mongodb.net/school?retryWrites=true&w=majority"
+require('dotenv').config();
 app.use(cors());
+mongoose.Promise = global.Promise
+
+mongoose.connect(dbUrl,{ useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log("Connect to MongoDB"))
+        .catch((err) => console.error("Error connecting to MongoDB:", err))
 
 app.get("/api/home",(req,res) => {
-    res.json({ message: "Connect MongoDB"});
+    res.json({ message: "hello"});
 });
 
 app.listen(PORT, () =>{

96a58c1aa596012e5fffb630e983ec88a7414690
jimimoss <lowzaokmi248@gmail.com>
2023-09-21 21:01:50 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const User = require('../models/Users')
-
 
 router.use(express.json())
 router.use(express.urlencoded({ extended: false }))
 
-router.get('/',async (req,res,next)=>{
+router.get('/register',async (req,res,next)=>{
     try {
-        const data = await User.find()
-        console.log(data)
-        res.json(data)
+        
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+        const user = new User({
+            fullname: req.body.fullname,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+
+        res.send(await user.save())
+
+        // const data = await User.find()
+        // console.log(data)
+        // res.json(data)
     } catch (err) {
         next(err)
     }

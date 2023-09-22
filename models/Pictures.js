b347148f43784be258e1cddfd758bed34c413bd1
eyefuu <eyefukng@gmail.com>
2023-09-23 00:51:13 +0700

    upupupupu

--- models/Pictures.js
+++ models/Pictures.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const pictureSchema = new mongoose.Schema({
+    SKUs_idSKUs:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SKUs'
+    },
     path: String,
     SKUs_idSKUs: {
         type: mongoose.Schema.Types.ObjectId,
--- models/SKUs.js
+++ models/SKUs.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const skusSchema = new mongoose.Schema({
+    Products_idProducts:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Products'
+    },
     color: String,
     goldWight: String,
     price: Number,
